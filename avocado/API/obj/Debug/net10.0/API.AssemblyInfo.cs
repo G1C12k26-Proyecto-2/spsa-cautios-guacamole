9c3eb6a3d1a88341fab1aeb92e19042d7db705c4
Aluxann <jlatorrel@ucenfotec.ac.cr>
2026-02-21 20:56:26 -0600

    Test, gtg

--- avocado/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ avocado/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d9b332d1fe77fd050cee689243406cfdb5223f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db3427bf7b28679fcf99d09c5e12d76dca7e49a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
